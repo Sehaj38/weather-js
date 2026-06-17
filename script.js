6a1079ba655aa608f33e202e894f6e1460054e85
Sehajneet kaur <kaursehajneet06@gmail.com>
2026-06-17 20:13:21 +0530

    Fix API key parameter in fetch URL

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const wind = document.getElementById("wind");
 
 async function getData(city) {
     const promise = await fetch(
-        `https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY=${city}&aqi=yes`
+        `https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q=${city}&aqi=yes`
     );
     return promise.json();
 }
